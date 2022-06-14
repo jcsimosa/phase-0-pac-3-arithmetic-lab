5d2a6f32829a516d66028060a586b1b9e2281017
Jose Simosa Nunez <josecarlossimosa@gmail.com>
2022-06-14 02:43:32 -0500

    commit tree e7c7439aa9a3959477412d579b38504bbfe9891e

---
+++ index.js
@@ -0,0 +1,25 @@
+function add(a,b){
+   return a + b
+};
+function subtract(b,a){
+   return b - a
+};
+function multiply(b,c){
+   return b * c
+};
+function divide(a,b){
+   return a / b
+};
+let n = 1
+function increment(n){
+    return (n+=  1)
+}
+function decrement(n){
+   return (n-= 1)
+}
+function makeInt(n){
+   return (parseInt(n,10))
+};
+function preserveDecimal(n){
+    return (parseFloat(n))
+}
